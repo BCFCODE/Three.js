79276477a1ba544aac68d4e40a726b43b8416135
Morteza Bakhshandeh <bcfcode@gmail.com>
2024-06-01 16:46:43 +0330

    WebGL compatibility check

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 import * as THREE from 'three';
+import WebGL from 'three/addons/capabilities/WebGL.js';
+
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -21,5 +23,17 @@ function animate() {
   cube.rotation.y += 0.01;
 
   renderer.render(scene, camera);
+
+}
+
+if (WebGL.isWebGLAvailable()) {
+
+  // Initiate function or other initializations here
+  animate();
+
+} else {
+
+  const warning = WebGL.getWebGLErrorMessage();
+  document.getElementById('container').appendChild(warning);
 
 }
